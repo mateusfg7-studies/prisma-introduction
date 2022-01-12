9bfb527f84ba4554bc7513a38e5633b077c8cbf3
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-12 14:55:54 -0300

    feat: create `src/index.ts`

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    id: 'user-1',
+    name: 'Mateus',
+    email: 'mateus@email.com',
+
+    groups: {
+      connectOrCreate: {
+        where: {
+          id: 'group-1',
+        },
+        create: {
+          id: 'group-1',
+          title: 'Group 1',
+        },
+      },
+    },
+  },
+})
