97b5c68e4c340b077a8e438aeddb7d31fc05efdc
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-12 15:34:07 -0300

    refactor: create `main` function

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,26 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {
-    id: 'user-1',
-    name: 'Mateus',
-    email: 'mateus@email.com',
+async function main() {
+  await prisma.user.create({
+    data: {
+      id: 'user-1',
+      name: 'Mateus',
+      email: 'mateus@email.com',
 
-    groups: {
-      connectOrCreate: {
-        where: {
-          id: 'group-1',
-        },
-        create: {
-          id: 'group-1',
-          title: 'Group 1',
+      groups: {
+        connectOrCreate: {
+          where: {
+            id: 'group-1',
+          },
+          create: {
+            id: 'group-1',
+            title: 'Group 1',
+          },
         },
       },
     },
-  },
-})
+  })
+}
+
+main()
